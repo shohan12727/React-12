1066b278f5fa412b6319fc1bfa6308455dc22c62
shohan12727 <aishohan001@gmail.com>
2025-09-20 23:50:06 +0600

    -----

--- rest-country/src/Components/Countries/Countries.jsx
+++ rest-country/src/Components/Countries/Countries.jsx
@@ -1,18 +1,28 @@
-import React, { use } from "react";
+import React, { use, useState } from "react";
 import Country from "../Country/Country";
 import "./Countries.css";
 const Countries = ({ countriesPromise }) => {
+  const [visitedCountries, setVisitedCountries] = useState([]);
+
+  const handleVisitedCountries = (country) => {
+    console.log("Clicked", country);
+  };
+
   const countriesData = use(countriesPromise);
   const mainElements = countriesData.countries;
 
   return (
     <div>
       <h1>In the country</h1>
+      <h3>Total Country Visited: </h3>
       <h3>All Country: {mainElements.length}</h3>
       <div className="countries">
         {mainElements.map((country) => (
-         
-          <Country  key={country?.ccn3.ccn3} country={country}></Country>
+          <Country
+            key={country?.ccn3.ccn3}
+            country={country}
+            handleVisitedCountries={handleVisitedCountries}
+          ></Country>
         ))}
       </div>
     </div>
--- rest-country/src/Components/Country/Country.jsx
+++ rest-country/src/Components/Country/Country.jsx
@@ -1,19 +1,22 @@
 import React, { useState } from "react";
 import "./Country.css";
 
-const Country = ({ country }) => {
+const Country = ({ country, handleVisitedCountries }) => {
   // console.log(country.area.area);
+
+  // console.log(handleVisitedCountries);
 
   const [visit, setVisit] = useState(false);
   const handleVisitedBtn = () => {
     setVisit(!visit);
+    handleVisitedCountries(country);
   };
-  
+
   return (
     <div className={`country ${visit && "country-visited"}`}>
       <img src={country.flags.flags?.png} alt={country.flags.flags?.alt} />
       <h2>Name: {country.name.common}</h2>
-      <h2>Population: {country.population.population}</h2>    
+      <h2>Population: {country.population.population}</h2>
       <p>
         Area: {country.area.area}{" "}
         {country.area.area > 30000 ? "Big Country" : "Small Country"}
