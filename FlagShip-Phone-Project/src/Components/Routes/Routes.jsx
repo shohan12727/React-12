39b2842925642784edc71111c2920c7fe2906bb1
shohan12727 <aishohan001@gmail.com>
2025-10-05 16:54:59 +0600

    basic rotue done

---
+++ FlagShip-Phone-Project/src/Components/Routes/Routes.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter } from "react-router";
+import Root from "../Pages/Root/Root";
+import ErrorPage from "../Pages/Error/ErrorPage";
+import Home from "../Home/Home";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        path: "/",
+        Component: Home,
+      },
+    ],
+  },
+]);
