af117c2d93629512656520a953cb01ed8557a7b4
shohan12727 <aishohan001@gmail.com>
2025-10-11 21:03:07 +0600

    meta tag and title added in 404 page

--- BoiPoka-Project/src/Components/Pages/Error/ErrorPage.jsx
+++ BoiPoka-Project/src/Components/Pages/Error/ErrorPage.jsx
@@ -1,9 +1,15 @@
 const ErrorPage = () => {
-    return (
-        <div>
-            <h1>Page not found</h1>
-        </div>
-    );
+  return (
+    <div>
+      <title>404 page</title>
+      <meta
+        name="description"
+        content="A brief description of your webpage for search engines."
+      />
+      <meta name="keywords" content="HTML, CSS, JavaScript, web development" />
+      <h1>Page not found</h1>
+    </div>
+  );
 };
 
 export default ErrorPage;
