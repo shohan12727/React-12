881102995f8f16fa5e5c98f55fa06bec6c216716
shohan12727 <aishohan001@gmail.com>
2025-10-21 01:04:43 +0600

    navigate added

--- dragon-news-project/src/Pages/Home.jsx
+++ dragon-news-project/src/Pages/Home.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import { Navigate } from 'react-router';
 
 const Home = () => {
     return (
-        <div>
-            home
-        </div>
+        <Navigate to="category/1"></Navigate>
+        // <h2>home</h2>
     );
 };
 
