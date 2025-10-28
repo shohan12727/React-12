dc9a146f2a00845785a4438521c21452a9162dad
shohan12727 <aishohan001@gmail.com>
2025-10-28 19:54:23 +0600

    post medhod with user in client side

--- user-management-client/src/Users.jsx
+++ user-management-client/src/Users.jsx
@@ -1,19 +1,40 @@
-import React, { use } from "react";
+import { use, useState } from "react";
 
 const Users = ({ userPromise }) => {
-  const userData = use(userPromise);
+  const initialUser = use(userPromise);
+  const [user, setuser] = useState(initialUser);
+
+  const handleAdduser = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    console.log(name, email);
+    const newUser = { name, email };
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newUser = [...user, data];
+        setuser(newUser);
+      });
+  };
 
   return (
     <div>
-      <form>
-        <input type="text" placeholder="Enter your name" />
+      <form onSubmit={handleAdduser}>
+        <input name="name" type="text" placeholder="Enter your name" />
         <br />
-        <input type="email" placeholder="Enter your email" />
+        <input name="email" type="email" placeholder="Enter your email" />
         <br />
         <button>Add user</button>
       </form>
 
-      {userData.map((user) => (
+      {user.map((user) => (
         <p key={user.id}>
           {user.name} Email: {user.email}
         </p>
