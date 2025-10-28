87f395700366b7aadf146ac2acff89d0d850c9ce
shohan12727 <aishohan001@gmail.com>
2025-10-28 08:30:53 +0600

    phone server side done

--- my-phone-server-side/index.js
+++ my-phone-server-side/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const phones = require("./phones.json")
 const app = express();
+const cors = require('cors');
 const port = 3000;
+app.use(cors());
 
 app.get('/', (req, res)=> {
    res.send("fuck yo yo  cse")
