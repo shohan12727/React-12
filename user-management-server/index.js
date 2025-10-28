1f5d766c5a324296a9b8132004a68e10ac97a636
shohan12727 <aishohan001@gmail.com>
2025-10-28 19:58:25 +0600

    post in server

--- user-management-server/index.js
+++ user-management-server/index.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("server is cooking");
+});
+
+const users = [
+  { id: 1, name: "sabana", email: "sabana@gmail.com" },
+  { id: 2, name: "sabnoor", email: "sabana@gmail.com" },
+  { id: 3, name: "sakila", email: "sabana@gmail.com" },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.post("/users", (req, res) => {
+  console.log(req.body);
+  const newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  res.send(newUser);
 });
 
 app.listen(port, () => {
