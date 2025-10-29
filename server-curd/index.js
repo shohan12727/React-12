a777b818dfaa36d4c274cf732d31b250807c1336
shohan12727 <aishohan001@gmail.com>
2025-10-29 22:21:53 +0600

    ___

---
+++ server-curd/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 3000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// mongo pass
+// h3C6dn6hhydR1wKk
+// curdUser
+
+const uri =
+  "mongodb+srv://curdUser:h3C6dn6hhydR1wKk@cluster0.alsn6h3.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Simple curd operation");
+});
+
+// MongoBD 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`port is running on ${port}`);
+});
