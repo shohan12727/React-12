1d0506eb952f83587e9fac5490af31f022b0f7ee
shohan12727 <aishohan001@gmail.com>
2025-11-03 15:05:07 +0600

    curd opetation in server side done

--- server-smart-deals/index.js
+++ server-smart-deals/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -26,6 +26,52 @@ app.get("/", (req, res) => {
 async function run() {
   try {
     await client.connect();
+
+    const smartDealsDB = client.db("smart_deals");
+    const smartDeals_collection = smartDealsDB.collection(
+      "smart_deals_collection"
+    );
+
+    app.get("/products", async (req, res) => {
+      const cursor = smartDeals_collection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await smartDeals_collection.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/products", async (req, res) => {
+      const newProducts = req.body;
+      const result = await smartDeals_collection.insertOne(newProducts);
+      res.send(result);
+    });
+
+    app.patch("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedProduct = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: {
+          name: updatedProduct.name,
+          price: updatedProduct.price,
+        },
+      };
+      const result = await smartDeals_collection.updateOne(query, update);
+      res.send(result);
+    });
+
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await smartDeals_collection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
