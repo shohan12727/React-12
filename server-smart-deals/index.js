d03b6925f3a3ddd98155e77a88c8e530298a26f8
shohan12727 <aishohan001@gmail.com>
2025-11-03 14:11:08 +0600

    server smart deals

---
+++ server-smart-deals/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://smartDeals_db:uI5JSxCuQ1RZht5f@cluster0.alsn6h3.mongodb.net/?appName=Cluster0";
+// mongo db client
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Smart server is running");
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`smart server is running on ${port}`);
+});
