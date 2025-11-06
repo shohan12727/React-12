691f686091318f0fc6eec5cc4bc8709f954e9c44
shohan12727 <aishohan001@gmail.com>
2025-11-06 13:40:21 +0600

    dotenv added

--- server-smart-deals/index.js
+++ server-smart-deals/index.js
@@ -1,15 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
-
+console.log(process.env);
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  "mongodb+srv://smartDeals_db:uI5JSxCuQ1RZht5f@cluster0.alsn6h3.mongodb.net/?appName=Cluster0";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.alsn6h3.mongodb.net/?appName=Cluster0`;
+
 // mongo db client
 const client = new MongoClient(uri, {
   serverApi: {
@@ -121,16 +123,32 @@ async function run() {
 
     app.get("/products/bids/:productId", async (req, res) => {
       const productId = req.params.productId;
-      const query = {product: productId};
-      const cursor= bidsCollection.find(query).sort({bid_price: -1})
+      const query = { product: productId };
+      const cursor = bidsCollection.find(query).sort({ bid_price: -1 });
       const result = await cursor.toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     app.get("/bids/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await bidsCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/bids", async (req, res) => {
+      const query = {};
+      if (query.email) {
+        query.buyer_email = email;
+      }
+      const cursor = bidsCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.delete("/bids/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bidsCollection.deleteOne(query);
       res.send(result);
     });
 
